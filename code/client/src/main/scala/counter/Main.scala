ffc68523d39f9bad270494a834d6502ac376e08d
Julien Richard-Foy <julien@richard-foy.fr>
2017-05-15 14:24:08 +0200

    This is it

--- code/client/src/main/scala/counter/Main.scala
+++ code/client/src/main/scala/counter/Main.scala
@@ -1,13 +1,48 @@
 package counter
 
-import scala.scalajs.js.JSApp
 import autowire._
-import scalajs.concurrent.JSExecutionContext.Implicits.queue
+import mhtml.{Var, mount}
+import org.scalajs.dom
+
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.scalajs.js.JSApp
+import scala.util.{Failure, Success}
 
 object Main extends JSApp {
 
   def main(): Unit = {
-    Client[ServiceDef].getCounter().call().onComplete(println)
+
+    val client = Client[ServiceDef]
+
+    client.getCounter().call().onComplete {
+      case Success(currentValue) =>
+
+        val app = {
+
+          val counter = Var(currentValue)
+
+          def onIncrement(): Unit = {
+            client.incrementCounter().call().foreach(x => counter := x)
+          }
+
+          def onReset(): Unit = {
+            client.resetCounter().call().foreach(x => counter := x)
+          }
+
+          <div style="text-align: center">
+            <h1>{ counter }</h1>
+            <div>
+              <button onclick={ onIncrement _ }>Increment</button>
+              <button onclick={ onReset _ }>Reset</button>
+            </div>
+          </div>
+        }
+
+        mount(dom.document.body, app)
+
+      case Failure(reason) => println("Unable to retrieve the counter state")
+    }
+
   }
 
 }
